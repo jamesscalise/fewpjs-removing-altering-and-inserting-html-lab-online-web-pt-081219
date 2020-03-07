600b07160cd133734732eca42eb4f3611de12405
James <jamescscalise@gmail.com>
2020-03-07 14:24:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+let doc = document
+
+
+let main = doc.querySelector('main')
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+
+document.body.appendChild(newHeader)
+
+newHeader.innerHTML = "James is the champion"
